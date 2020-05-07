db3dec1074464f70f059f991ffdac7683749983f
Saad Aljammaz <saad@Saads-MacBook-Pro.local>
2020-05-07 09:31:17 +0300

    change search functions

--- app.py
+++ app.py
@@ -149,14 +149,24 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+  search_term = request.form.get('search_term', '')
+  venues = Venue.query.filter(Venue.name.ilike(f'%{search_term}%')).all()
+  response = {
+    "count": len(venues),
+    "data": []
   }
+  for venue in venues:
+    num_upcoming_shows = 0
+    shows = Show.query.filter_by(venue_id=venue.id).all()
+    for show in shows:
+      if show.start_time > datetime.now():
+        num_upcoming_shows += 1
+
+    response['data'].append({
+      "id": venue.id,
+      "name": venue.name,
+      "num_upcoming_shows": num_upcoming_shows,
+    })
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -316,15 +326,25 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  search_term = request.form.get('search_term', '')
+  artists = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).all()
+  response = {
+    "count": len(artists),
+    "data": []
   }
-  artist = Artist.query.get()
+  for artist in artists:
+    num_upcoming_shows = 0
+    shows = Show.query.filter_by(artist_id=artist.id).all()
+    for show in shows:
+      if show.start_time > datetime.now():
+        num_upcoming_shows += 1
+
+    response['data'].append({
+      "id": artist.id,
+      "name": artist.name,
+      "num_upcoming_shows": num_upcoming_shows,
+    })
+  
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
@@ -566,7 +586,7 @@ def create_shows():
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
+  # TODO: insert form data as a new Show record in the db, instead | Done
   try:
     artist_id = request.form['artist_id']
     venue_id = request.form['venue_id']
